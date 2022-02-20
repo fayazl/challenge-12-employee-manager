17277c7473c1af5e09edbe06f703a094182ccab2
Fayaz <flalani100@gmail.com>
2022-02-20 00:06:02 -0500

    Added two new tables department and role with constraint

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,14 @@
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT Null);
+    name VARCHAR(30) NOT NULL);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL 
+);
