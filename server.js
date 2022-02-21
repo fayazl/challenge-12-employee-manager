b1160642645a6690652ba79510e904afe11f0849
Fayaz <flalani100@gmail.com>
2022-02-21 01:06:13 -0500

    attempting to use switch and case function

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connection = require('./db/connection');
 const query = require('./query');
 const cTable = require('console.table');
 const Choices = require('inquirer/lib/objects/choices');
+const allDepartments = require('./query');
 
 inquirer
     .prompt([
@@ -13,3 +14,15 @@ inquirer
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role' ]
         }
     ])
+
+    .then(answers => {
+
+        switch (answers.option) {
+            case 'View all departments':
+                allDepartments().then(data => {
+                    console.table(data)
+                })
+
+        }
+
+    })
