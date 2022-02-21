00c73f1f506dfdf5048c9562b76b3ea29c146bea
Fayaz <flalani100@gmail.com>
2022-02-20 22:22:05 -0500

    Added all roles and all employees query

--- query.js
+++ query.js
@@ -2,13 +2,51 @@ const db = require('./db/connection');
 const mysql = require('mysql2');
 const cTable = require('console.table')
 
-
-const sql = 'SELECT * FROM department';
-
+function allDepartments(){
+    const sql = `SELECT * FROM department`;
     db.promise().query(sql).then(([rows, fields]) => {
         console.table(rows)
 
     });
+}
+
+function allRoles(){
+    const sql = `SELECT role.id, role.title AS 'role', department.name AS 'department', salary, department.id
+    AS department_id
+    FROM role
+    LEFT JOIN department
+    ON role.department_id = department.id`
+
+    db.promise().query(sql).then(([rows, fields]) => {
+        console.table(rows)
+    })
+}
+
+function allEmployees(){
+    const sql = `SELECT employee.id, employee.first_name AS 'first name', employee.last_name AS 'last name', 
+    role.title AS 'title', role.salary, department.name AS 'department', 
+    (SELECT m.first_name FROM employee m WHERE m.id = employee.manager_id) 
+    AS 'manager'
+    FROM employee
+    LEFT JOIN role 
+    ON role.id = employee.role_id
+    LEFT JOIN department
+    ON department.id = role.department_id`
+
+    db.promise().query(sql).then(([rows, fields]) => {
+        console.table(rows)
+    })
+}
+
+// function addDepartment
+
+
+
+allDepartments();
+allRoles()
+allEmployees()
+
+
 
 
 
@@ -47,4 +85,4 @@ const sql = 'SELECT * FROM department';
 
 // }
 
-// module.exports = Database; 
+// module.exports = Database;
